ddfd9bc095cf53c1d1c2738f4c274010ca8cb924
tarang upadhyay <tarangupadhyay26@gmail.com>
2025-11-24 15:00:09 +0530

    Remove dist and parcel-cache from Git tracking

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return(
+    <div className="header">
+    <div className="logo-container">
+      <img
+        className="logo"
+        src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+        alt="logo"
+      />
+    </div>
+    <div className="nav-items">
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+  );
+};
+
   
 
-// const element =()=>(
-//     <h1 id="header">Tarang</h1>
-// )
+const AppLayout = () => {
+  return <div className="app">
+    <Header/>
+  </div>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<AppLayout/>);
